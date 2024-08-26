27da68b0742384f68e610f0a825c593a8e0d7550
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-26 15:06:09 -0700

    Create Balloon.js

---
+++ components/Balloon.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Balloon from "./components/Balloon.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Balloon />
+    </div>
+  );
+}

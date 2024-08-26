a4644e685e1be07348b562b82916891876beb5d2
LeoKnox <LeoKnox@users.noreply.github.com>
2024-08-26 15:46:03 -0700

    Update Balloon.js

--- components/Balloon.js
+++ components/Balloon.js
@@ -6,18 +6,38 @@ export default Balloon = () => {
   return (
     <div>
       <svg viewBox={`0 0 ${balloonWidth} ${balloonHeight + threadHeight}`}>
+        <defs>
+          <radialGradient
+            id="balloon-gradient"
+            cx="40%"
+            cy="40%"
+            r="50%"
+            fx="30%"
+            fy="30%"
+          >
+            <stop offset="0%" stopColor="#fff" />
+            <stop offset="100%" stopColor="currentColor" />
+          </radialGradient>
+        </defs>
         <rect
           x={balloonWidth / 2}
           y={balloonHeight}
           width="1"
           height={threadHeight}
-          fill="red"
+          fill="currentColor"
         />
         <polygon
           points={`${balloonWidth / 2},${balloonHeight - 3} ${
             balloonWidth / 2 + 8
           },${balloonHeight + 5} ${balloonWidth / 2 - 8},${balloonHeight + 5}`}
-          fill="red"
+          fill="currentColor"
+        />
+        <ellipse
+          cx={balloonWidth / 2}
+          cy={balloonHeight / 2}
+          rx={balloonWidth / 2}
+          ry={balloonHeight / 2}
+          fill="url(#balloon-gradient)"
         />
       </svg>
     </div>
